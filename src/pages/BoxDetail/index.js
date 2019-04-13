9c8264684549bc2c3156329e14c0aef02c11565e
tapachec0 <talytapacheco.1c2015@gmail.com>
2019-04-13 02:37:42 -0300

    Added Real Time funcionality

--- src/pages/BoxDetail/index.js
+++ src/pages/BoxDetail/index.js
@@ -24,8 +24,8 @@ export default class BoxDetail extends Component {
         const box = this.props.match.params.id;
         const io = socket('https://semana-rockseat-backend.herokuapp.com');
         io.emit('connectRoom', box);
-        io.on("file", data =>{
-            this.setState({box: { ...this.state.box, file: [data, ...this.state.box]}})
+        io.on('file', data =>{
+            this.setState({box: { ...this.state.box, files: [data, ...this.state.box.files]}})
         });
     }
 
@@ -34,7 +34,7 @@ export default class BoxDetail extends Component {
             const data = new FormData();
             const box = this.props.match.params.id;
             data.append('file', file);
-            api.post(`/boxes${box}/files`, data);
+            api.post(`boxes/${box}/files`, data);
         });
     };
   render() {
